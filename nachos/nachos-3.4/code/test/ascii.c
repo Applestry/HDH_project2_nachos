bf440065f7e9480d8844131f538f1b0b9f5a2999
TuMinz <129361284+Tuminz@users.noreply.github.com>
2024-03-30 23:42:38 +0700

    Update ascii.c

--- nachos/nachos-3.4/code/test/ascii.c
+++ nachos/nachos-3.4/code/test/ascii.c
@@ -2,17 +2,44 @@
 
 int main()
 {
-	int i;
+	int i, id, cnt, temp;
+	char fileName[] = "ascii.txt";
+	char buffer[1000];
 
+	if (CreateFile(fileName) == 0)
+	{
+		PrintString("Tao file thanh cong!\n");
+		id = Open(fileName, 0);
+	}
 	PrintString("Bang ma ASCII la: ");
 	
-	for (i = 0; i <= 127; i++)
+	cnt = 0;
+	for (i = 32; i <= 127; i++)
 	{
-		PrintChar((char)i);
-		PrintChar(' ');
+		temp = i;
+		while (temp > 0)
+		{
+			buffer[cnt++] = (temp % 10) + '0';
+			temp /= 10;
+		}
+		if (i >= 100)
+		{
+			temp = buffer[cnt - 1];
+			buffer[cnt - 1] = buffer[cnt - 3];
+			buffer[cnt - 3] = temp;
+		}
+		else
+		{
+			temp = buffer[cnt - 1];
+			buffer[cnt - 1] = buffer[cnt - 2];
+			buffer[cnt - 2] = temp;
+		}
+		buffer[cnt++] = ' ';
+		buffer[cnt++] = i;
+		buffer[cnt++] = '\n';
 	}
-	PrintChar('\n');
-	PrintString("_______________");
-
+	PrintString(buffer);
+	Write(buffer, cnt, id);
+	Close(id);
 	return 0;
 }
